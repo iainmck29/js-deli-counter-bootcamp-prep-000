970f12354d504b3caba3682c0e355645efa39986
Iain McKenzie <iainmck29@hotmail.com>
2018-09-03 13:19:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-function takeANumber(current, number) {
-  var array = [];
-  for (var i = 0; i < array.length; i++) {
-    number = i + 1;
-    array.push(`Welcome, ${current}. You are number ${number} in line`)
-  } return array.splice(i, 1);
+function takeANumber(line, name){
+  line.push(name);
+  
+  console.log("Welcome, " + name + ". You are number " + line.length + " in line.");
+  
+  return "Welcome, " + name + ". You are number " + line.length + " in line."
 }
