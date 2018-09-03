b0630eb20f32368489d6abd4c1903d7f842bbb4b
Iain McKenzie <iainmck29@hotmail.com>
2018-09-03 13:16:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function takeANumber(current, number) {
+  var array = [];
   for (var i = 0; i < array.length; i++) {
     number = i + 1;
     array.push(`Welcome, ${current}. You are number ${number} in line`)
