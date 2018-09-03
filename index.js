7b560c2c7df3d2bcbfc72e4ba8d067ca650bf6ab
Iain McKenzie <iainmck29@hotmail.com>
2018-09-03 13:56:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function takeANumber(line, name){
 
 function nowServing(line) {
   if (line.length === 0) {
-    return `The line is currently empty.`
+    return `There is nobody waiting to be served!`
   } 
     return "Currently serving " + line.shift() + ".";
   
