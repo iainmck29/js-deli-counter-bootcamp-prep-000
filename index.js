f56ce8e4215756790038ee508746f4d0fb7497de
Iain McKenzie <iainmck29@hotmail.com>
2018-09-03 14:06:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ function currentLine(line) {
   if (line.length === 0) {
     return `The line is currently empty.`
   } 
-  for (i=)
-    return `The line is currently: ` + 
+  for (var i = 1; i < line.length; i++) {
+    return `The line is currently: ` + i + ". " + 
+  }
+    
 }
